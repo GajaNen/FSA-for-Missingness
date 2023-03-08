fc4a0c983e9e83ec24c200c2870ea463e6bdbe0a
GajaNen <33069162+GajaNen@users.noreply.github.com>
2023-03-08 02:19:39 +0100

    updated dependencies, added GA, SA and Boruta algorithms

--- Data/runAlgo.R
+++ Data/runAlgo.R
@@ -2,7 +2,7 @@
 
 fitAlgo <- function(func, name, params, folds, X, Y){
   
-  R2r <- NA
+  #R2r <- NA
   regs <- name %in% c("lasso", "EN", "LR")
   kTr <- regs * params$kOut + (!regs) * params$kInn
   nTR <- 1 + (name == 'EN')
@@ -12,12 +12,12 @@ fitAlgo <- function(func, name, params, folds, X, Y){
                                    classProbs = T, index = foldsTr)
   if (regs){
     
-    model <- caret::train(x=X, y=Y, family = "binomial",
+    model <- caret::train(x = X, y = Y, family = "binomial",
                           method = func, trControl = trControl, 
-                          tuneGrid = tuneGrid, tuneLength = 40)
+                          tuneGrid = tuneGrid)
     if (name == "LR") {
       # nam: coefficients of the final optimal model/HP fitted to the whole train set
-      R2r <- performance::r2_mckelvey(model$finalModel)
+      #R2r <- performance::r2_mckelvey(model$finalModel)
       nam <- names(which((coef(summary(model))[-1,"Pr(>|z|)"]) < params$pGLM))
       
     } else nam <- names(which(coef(model$finalModel, model$bestTune$lambda)[-1,] != 0))
@@ -28,21 +28,58 @@ fitAlgo <- function(func, name, params, folds, X, Y){
     
   } else {
     
-    ctrl <- caret::rfeControl(functions = params[[name]], method = "cv",
-                              number = params$kOut, index = folds)
-    res <- caret::rfe(x=X, y=Y,
-                      sizes = params$sizes,
-                      rfeControl = ctrl,
-                      trControl = trControl,
-                      tuneGrid=tuneGrid,
-                      method = func)
-    indices <- extrReg(predictors(res))
-    # mean cross-validated pf
-    acc <- res$results[res$results$Variables==res$bestSubset,"Accuracy"]
+    # change that to predefined structure
+    preds <- rep(list(), 3)
+    acc <- list()
+    
+    # RFE
+    ctrlRFE <- caret::rfeControl(functions = params[[paste0(name, "rfe")]], 
+                                 method = "cv", number = params$kOut, 
+                                 index = folds)
+    resRFE <- caret::rfe(x = X, y = Y,
+                         sizes = params$sizes,
+                         rfeControl = ctrlRFE,
+                         trControl = trControl,
+                         tuneGrid = tuneGrid,
+                         method = func)
+    preds$rfe <- predictors(resRFE)
+    acc$rfe <- resRFE$results[resRFE$results$Variables==resRFE$bestSubset,"Accuracy"]
+    
+    # GA
+    ctrlGA <- caret::gafsControl(functions = params[[paste0(name, "ga")]], 
+                                 method = "cv", number = params$kOut, 
+                                 index = folds)
+    resGA <- caret::gafs(x = X, y = Y,
+                         iters = 10,
+                         elite = 1,
+                         gafsControl = ctrlGA,
+                         method = func,
+                         tuneGrid = tuneGrid)
+    preds$ga <- resGA$optVariables
+    acc$ga <- resGA$averages[resGA$optIter,"Accuracy"]
+    
+    # SA
+    ctrlSA <- caret::safsControl(functions = params[[paste0(name, "sa")]], 
+                                 method = "cv",number = params$kOut, 
+                                 index = folds, improve = 50)
+    resSA <- caret::safs(x = X, y = Y,
+                         iters = 100,
+                         safsControl = ctrlSA,
+                         method = func,
+                         tuneGrid = tuneGrid)
+    preds$sa <- resSA$optVariables
+    acc$sa <- resSA$averages[resSA$optIter, "Accuracy"]
+    
+    # Boruta for RF
+    if (name == "RF") {
+      mod <- Boruta(Y~X)
+      predB <- names(mod$finalDecision[mod$finalDecision == "Confirmed"])
+      if (length(predB)) preds$Boru <- paste0("X", predB, "D") else NULL
+      # mean cross-validated pf
+    }
     
   }
   
-  preds <- if (length(indices)) paste0("X", indices, "D") else NULL
   return(list(acc=acc, preds=preds, R2r=R2r))
   
 }
