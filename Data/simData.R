2ab447670b7612122def8aa323ff377541d063c3
GajaNen <33069162+GajaNen@users.noreply.github.com>
2023-04-01 17:13:35 +0200

    added Gaussian copula for corr preds and removed redundant features

--- Data/simData.R
+++ Data/simData.R
@@ -1,69 +1,70 @@
 ###--------------------------------------------------------------------------###
 
+# simulate correlated or independent relevant and irrelevant predictors
+
 simX <- function(params){
   
-  # generate relevant independent X
-  Nrel <- params$percRel * params$Ntotal
-  Nrest <- params$Ntotal - Nrel
-  if ((Nrel %% 3) || (Nrest %% 3)) stop(cat("Number of relevant and
-                                        the rest of features must
-                                        be a multiple of 3."))
-  relt <- cbind(sapply(params$normParam, 
-                      function(x) rnorm(params$N, x[1], x[2]),
-                      simplify = T),
-               sapply(params$gammaParam, 
-                      function(x) rgamma(params$N, shape=x[1], rate=x[2]),
-                      simplify = T),
-               sapply(params$probBinRel, 
-                      function(x) rbinom(params$N, 1, x),
-                      simplify = T),
-               sapply(1:(Nrel / 3), 
-                      function(x) sample(0:(params$ncatsRel[x]-1), 
-                                         params$N, 
-                                         replace = TRUE, 
-                                         prob = params$probOrdRel[[x]]),
-                      simplify = T))
-  colnames(relt) <- paste0("relevan", 1:ncol(relt))
-  # generate redundant features
-  if (params$typeRest == "IR"){
-    Nred <- Nrel
-    redt <- relt # add names
-    # add noise 
-    colnames(redt) <- paste0("redund", 1:ncol(redt))
+  Nrel <- params$pr * params$Ntotal
+  if ((Nrel %% 6) || (Nrest %% 6)) stop("Number of relevant and
+                                        the irrelevant features each must
+                                        be a multiple of 6. Adjust Ntotal
+                                        or pr.")
+  if (params$corrPred){
+    paramMarg <- c(unlist(params$normParamRel, recursive = F),
+                   unlist(params$gamParamRel, recursive = F),
+                   unlist(params$binParamRel, recursive = F),
+                   lapply(1:(Nrel / 3),
+                          function(x) c(location = 0, scale = 1)))
+    copula <- copula::normalCopula(param = unlist(params$cormat), 
+                                   dim = Nrel,
+                                   dispstr = "un")
+    joint <- copula::mvdc(copula = copula, 
+                          margins = unlist(params$marginals), 
+                          paramMargins = lapply(paramMarg, function(x) as.list(x)))
+    relt <- as.data.frame(copula::rMvdc(1000, joint))
+    baseprobs <- genProbs(params$popProbsRel[[1]])
+    cprop <- t(apply(baseprobs, 1, cumsum)) 
+    quant <- t(apply(cprop, 1, stats::qlogis))
+    relt[, (Nrel - Nrel / 3 + 1): Nrel] <- 
+      genOrd(logisZ = relt[, (Nrel - Nrel / 3 + 1): Nrel, drop = F], 
+             quant = quant,
+             N = params$N,
+             nvar = (Nrel / 3))
   } else {
-    Nred <- 0
-    redt <- vector(length = params$N)
+    relt <- genIndep(nP = unlist(params$normParamRel, recursive = F),
+                     gP = unlist(params$gamParamRel, recursive = F),
+                     bP = unlist(params$binParamRel, recursive = F),
+                     lP = params$popProbsRel[[1]],
+                     Nobs = params$N,
+                     Nvar = Nrel)
   }
+  colnames(relt) <- paste0("rel", 1:ncol(relt))
   # generate irrelevant
-  irrelt <- cbind(sapply(params$normParam, 
-                        function(x) rnorm(params$N, x[1], x[2]),
-                        simplify = T),
-                 sapply(params$gammaParam, 
-                        function(x) rgamma(params$N, shape=x[1], rate=x[2]),
-                        simplify = T),
-                 sapply(params$probBinIrrel, 
-                        function(x) rbinom(params$N, 1, x),
-                        simplify = T),
-                 sapply(1:(Nrest - Nred), 
-                        function(x) sample(0:(params$ncatsIrrel[x]-1), 
-                                           params$N, 
-                                           replace = TRUE, 
-                                           prob = params$probOrdIrrel[[x]]),
-                        simplify = T))
-  colnames(irrelt) <- paste0("irrelvan", 1:ncol(irrelt))
-  
-  # is sample the correct way? i.e. are probs the cumulative probs
-  
-  return(data.frame(cbind(relt, irrelt, redt)))
+  irrelt <- genIndep(nP = unlist(params$normParamIrrel, recursive = F),
+                     gP = unlist(params$gamParamIrrel, recursive = F),
+                     bP = unlist(params$binParamIrrel, recursive = F),
+                     lP = params$popProbsIrrel[[1]],
+                     Nobs = params$N,
+                     Nvar = params$Ntotal - Nrel)
+  colnames(irrelt) <- paste0("irrel", 1:ncol(irrelt))
+
+  return(data.frame(cbind(relt, irrelt)))
   
 }
 
 ###--------------------------------------------------------------------------###
 
+# simulate Y for a given R^2 Y~X
+# simulate Y for correlations expected given the population correlation
+
 simY <- function(params, X){
   
   rey <- cor(X[,1:(params$percRel * params$Ntotal)], 
              X[,1:(params$percRel * params$Ntotal)])
+  # make this the population matrix -- this way it will always be SPD
+  # and the correlations will be theoretical, e.g. the R2 will be theoretical
+  # and so the actual R2 witht thi data will change with the deviations from
+  # the correlations
   cors <- sample(1:10, (params$percRel * params$Ntotal), replace = T)
   a <- sqrt(params$R2y/(t(cors) %*% solve(rey) %*% cors))[1]
   cors <- a*cors
@@ -75,54 +76,5 @@ simY <- function(params, X){
   sigma2 <- c((1 - cors %*% cov(X.dual) %*% cors) / var(e))
   return(X.dual %*% cors + sqrt(sigma2)*e)
 }
-
-###--------------------------------------------------------------------------###
-
-simRho <- function(params){
-  
-  # sample from LKJ distribution
-  rey <- rethinking::rlkjcorr(1, params$nX, params$eta)
-  diag(rey) <- 1
-  
-  # move cat y to the beginning: requirement for simmulticorr package
-  vars_move <- paste0("Y", which(params$typeY == "cat"), "D")
-  preds <- rep(list(rep(NA, sum(params$infoX)/params$nY)), params$nY)
-  
-  for (j in 1:params$nY){
-    
-    corX <- rep(0, params$nX)
-    names(corX) <- paste0("X", 1:params$nX, "D")
-    
-    nCat <- params$infoX[1]/params$nY
-    nCont <- params$infoX[2]/params$nY
-    end_non <- params$infoX[1]+params$infoXnon[1]
-    
-    # each Y has different X predictors to get different patterns
-    preds[[j]] <- paste0("X", 
-                         c((1:(nCat)) + (nCat)*(j-1), 
-                           ((end_non+1):(end_non+nCont)) + (nCont)*(j-1)), 
-                         "D")
-    corX[preds[[j]]] <- runif(length(preds[[j]]), 4, 6)
-    # each y_i uncorrelated with every y_j
-    cors <- c(corX, rep(0, j-1))
-    n <- params$nX+j-1
-    # generate corr between X and Y for a given R2 (defined in terms of deviance)
-    a <- sqrt(params$R2y/(t(cors) %*% solve(rey[1:n,1:n]) %*% cors))[1]
-    cors <- a*cors
-    rey <- cbind(rbind(rey, cors), c(t(cors), 1))
-    
-    colnames(rey)[params$nX+j] <- paste0("Y", j, "D")
-    
-    
-    
-  }
-  
-  rownames(rey) <- colnames(rey)
-  rey <- reorder_mat(rey, c(vars_move, setdiff(colnames(rey), vars_move)))
-  
-  return(list(rey=rey, preds=preds))
-  
-}
-
 
 ###--------------------------------------------------------------------------###
