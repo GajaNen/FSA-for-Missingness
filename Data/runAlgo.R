158aebe186052424cacfea6e78b38033f2a07211
GajaNen <33069162+GajaNen@users.noreply.github.com>
2023-04-03 21:19:15 +0300

    debugged algorithm fitting

--- Data/runAlgo.R
+++ Data/runAlgo.R
@@ -1,11 +1,6 @@
 ###--------------------------------------------------------------------------###
 
-# this is an ugly and long function
-# but making a smaller one and than using a form of looping, e.g. (x)apply, for
-# each algorithm I want to apply is the same, if not worse
-# open to other suggestion though 
-
-fitAlgo <- function(params, X, target){
+fitAlgo <- function(params, X, target, rpt){
   
   ranks <- data.table::data.table(array(
     data = NA, dim = c(ncol(X), length(params$rankers))))
@@ -35,7 +30,7 @@ fitAlgo <- function(params, X, target){
   trControl <- caret::trainControl(method = "cv", number = params$kInn,
                                    selectionFunction = "oneSE")
   
-  for (name in intersect(names(all), c("rfRFE", "linSvmRFE", "rbfSvmRFE"))){
+  for (name in intersect(names(all), c("rfRFE", "rbfSvmRFE"))){
     
     ctrlRFE <- caret::rfeControl(functions = unlist(ifelse(grepl("^rf", name),
                                                     list(rfFuncs), 
@@ -49,35 +44,31 @@ fitAlgo <- function(params, X, target){
                          tuneGrid = params$tuneGrids[[all[[name]]]],
                          method = all[[name]])
     substs[[name]] <- predictors(resRFE)
-    # these ranks are only when variables were selected
-    # I want to obtain averaged ranking over 5 folds obtained before fitting 
-    # each subset size
-    #rk <- aggregate(resRFE$variables[,"Overall"], list(resRFE$variables$var), mean)
-    #ranks[,name] <- rk[as.integer(sapply(colnames(X),function(x) which(x == rk[,1]))),"x"]
     acc[[name]] <- resRFE$results[resRFE$results$Variables==resRFE$bestSubset,"Accuracy"]
   }
   
-  for (name in intersect(names(all), (c("rfSA", "rbfSvmRFE")))){
+  for (name in intersect(names(all), (c("rfSA", "linSvmSA", "rbfSvmSA")))){
     
     ctrlSA <- caret::safsControl(functions = unlist(ifelse(grepl("^rf", name),
                                                            list(rfSA), 
                                                            list(caretSA))),
-                                 method = "cv",number = params$kOut, 
-                                 index = folds)
+                                 method = "cv", number = params$kOut, 
+                                 index = folds, improve = 10)
     resSA <- caret::safs(x = X, y = target,
-                         iters = 50,
+                         iters = 10,
                          safsControl = ctrlSA,
                          method = all[[name]],
-                         tuneGrid = NULL,
-                         trControl = trControl)
+                         tuneGrid = NULL, # too long otherwise
+                         trControl = trControl,
+                         differences = F)
     substs[[name]] <- resSA$optVariables
     acc[[name]] <- resSA$averages[resSA$optIter, "Accuracy"]
   } 
-    
-  #filt_eval <- FSinR::filterEvaluator('ReliefFeatureSetMeasure')
-  #aco_search <- FSinR::antColony()
-  #res <- aco_search(cbind(X, target), "target", featureSetEval = filt_eval)
-  #preds <- dimnames(res$bestFeatures)[[2]][res$bestFeatures==1]
+  
+  filt_eval <- FSinR::filterEvaluator('giniIndex')
+  hyb_search <- FSinR::whaleOptimization(10, 50)
+  res <- hyb_search(cbind(X, target), "target", featureSetEval = filt_eval)
+  substs[["hyb"]] <- dimnames(res$bestFeatures)[[2]][res$bestFeatures==1]
 
   ids <- rep(NA, params$N)
   for (x in 1:params$kOut) ids[setdiff(1:params$N, folds[[x]])] <- x
@@ -93,36 +84,25 @@ fitAlgo <- function(params, X, target){
   
   # check sample size parameter
   ranks[,"ReliefF"] <- FSelectorRcpp::relief(x=X, y=target, sampleSize = 10)[,2]
-  #ranks_stir <- stir::stir(X) maybe if not too slow
   
-  # check how to add nominal: e.g. if you change name, ^.bin, ^.ord
-  # and save for each threshold a subset 
-  # FCBC_thres1
-  # and so add the number of subsets for (n-1)+n_thresholds_fcbc
-  lapply(params$fcbcThres, 
-         function(x) Biocomb::select.fast.filter(cbind(X, target),
-                                                      disc.method = "MDL",
-                                                      threshold = x,
-                                                      attrs.nominal = c(9:24, 57:120)))
-  return(list(ranks, substs))
+  substs <- c(substs, setNames(lapply(params$fcbcThres, 
+                                      function(x) Biocomb::select.fast.filter(
+                                        cbind(X, target),
+                                        disc.method = "MDL",
+                                        threshold = x,
+                                        attrs.nominal = which(grepl("(.*Bin)|(.*Ord)", 
+                                                                    colnames(X))))),
+                                paste0("FCBC_", params$fcbcThres)))
   
+  saveRDS(list(ranks, substs, acc), paste0("mech_", params$mechanism,
+                                           "_pm_", params$pm,
+                                           "_corrPred_", params$corrPred,
+                                           "_pr_", params$pr,
+                                           "_rep_", rpt))
 }
 
 ###--------------------------------------------------------------------------###
-
-simCond(params, seed, rpt){
-  
-  # add magic with seeds
-  X <- simX(params)
-  Y <- simY(params, X[,grepl("^rel", colnames(X))])
-  R <- factor(simR(params, X, Y), labels = c("c", "m"))
-  res <- fitAlgo(params, X, R)
-  saveRDS(res, paste0(""))
-  
-}
-
-###--------------------------------------------------------------------------###
-
+              
 simRep <- functio(fixed, varied, seed, rp){
   
   for (i in 1:nrow(varied)){
@@ -135,9 +115,5 @@ simRep <- functio(fixed, varied, seed, rp){
     saveRDS(res, paste0(""))
   }
 }
-
-# make a loop over conditions (or rather rows of variedParams)
-# and take care of the RNG -- simCond not even needed if I use foreach
-# and then for over varied and within this
 
 ###--------------------------------------------------------------------------###
