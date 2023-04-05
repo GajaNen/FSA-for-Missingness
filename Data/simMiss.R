dc880df7ddcaae1ec9f1d51ac319f49a1777e64c
GajaNen <33069162+GajaNen@users.noreply.github.com>
2023-04-05 16:25:19 +0300

    further swithcing to data.table + simplifying

--- Data/simMiss.R
+++ Data/simMiss.R
@@ -1,19 +1,13 @@
 ###--------------------------------------------------------------------------###
 
-# generate a cubic spline with 1 knot at the median
+# generate a spline with 1 knot at the median for one variable
 
-simSpline <- function(dat, knots=1, deg=4, beta=NULL){
+simSpline <- function(x, deg=3, knots=1, coefSpl=NULL){
   
-  if (is.null(dim(dat))) stop("Data has to be 2D.")
-  spl <- apply(dat, 2, function(x) {
-    if (is.null(beta)) beta <- sample(0:4, deg + knots + 1, replace=T)
-    basis <- splines::bs(x = x, knots = median(x), degree = deg, intercept = TRUE)
-    basis %*% beta
-    }
-  )
-  
-  return(spl)
-   # this could also be performed on columns of datatable! in the main func
+  if (is.null(coefSpl)) coefSpl <- rep(1, deg + knots + 1)
+  bas <- splines::bs(x = x, knots = median(x), degree = deg, intercept = TRUE)
+  return(as.vector(bas %*% coefSpl))
+
 }
 
 ###--------------------------------------------------------------------------###
@@ -21,30 +15,28 @@ simSpline <- function(dat, knots=1, deg=4, beta=NULL){
 simProbit <- function(params, Xrel, Y){
 
   z <- qnorm(1-params$pm)
-  out <- data.table(LP=rep(NA, params$N))
+  out <- data.table::data.table(LP=rep(NA, params$N))
   is.mnar <- params$mechanism == "mnar"
 
-  # apply splines to cont vars and y if mnar
-  # to do: y is data.table, make it empty if condition not satisfied
+  # apply splines to cont vars (and y if is.mnar)
   contNms <- names(Xrel)[grepl("^RelCont", names(Xrel))]
-  splns <- simSpline(cbind(
-    Xrel[,..contNms],
-    Y[is.mnar]),
-    deg = params$deg)
+  splns <- cbind(data.table::copy(Xrel[,..contNms]), Y[,..is.mnar]) 
+  splns[, (contNms) := lapply(.SD, simSpline, deg = params$deg),
+        .SDcols = contNms]
   
-  # apply some transformations to any discrete X (also ordinal with ncat > 5)
-  catNms <- names(Xrel)[grepl("(^RelBin)|(^RelOrd)", names(Xrel))]
-  if (is.null(params[["trans"]])){
-    transf <- lapply(1:length(catNms), function(x) identity) 
-  } else transf <- params[["trans"]]
-  transformed <- data.table::copy(Xrel[,..catNms]) # not copied by reference!
-  dim(transformed[, catNms := Map({function(f, x) f(x)}, transf, .SD), .SDcols = catNms])
-  #dim(transformed[, ..catNms])
+  # apply some transformations to any discrete X
+  catNms <- names(Xrel)[grepl("(^RelBin)|(^RelOrd)", names(Xrel))] # for safety
+  if (is.null(params$trans)){
+    transf <- lapply(1:length(catNms), function(x) identity)
+  } else transf <- params$trans
+  transformed <- data.table::copy(Xrel[,..catNms])
+  transformed[, (catNms) := mapply({function(f, x) f(x)}, transf, .SD, SIMPLIFY = FALSE), 
+              .SDcols = catNms]
   
   preds.all <- cbind(splns, transformed)
   # Define a slope vector (if y.pred make it larger)
   beta <- runif(ncol(preds.all), 1, 2)
-  if (predsY) { 
+  if (is.mnar) { 
     beta <- c(beta, runif(ncol(Y.preds), max(beta)*10, max(beta)*20))
     preds.all <- cbind(preds.all, Y.preds)
   }
@@ -53,17 +45,17 @@ simProbit <- function(params, Xrel, Y){
   out[, LP := as.matrix(preds.all) %*% beta]
   
   # define residual variance for the linear predictor
-  sig <- t(beta) %*% cov(preds.all) %*% beta
+  s <- t(beta) %*% cov(preds.all) %*% beta
   if (params$R2r %in% c(0,1)) stop('R**2 for indicator must not be 0 or 1.')
-  resVar <- (sig / params$R2r) - sig
+  resSig <- (s / params$R2r) - s
   
   # define theoretical sd of lp
-  sd_LP <- as.numeric(sqrt(sig + resVar))
+  sd.LP <- as.numeric(sqrt(s + resSig))
     
   # set mean as threshold and discretize at z*theoretical SD above it
-  thresh <- mean(out)
-  LPs <- out + MASS::mvrnorm(params$N, 0, resVar)
-  return(LPs > (thresh + z*sd_LP))
+  out[, thresh := mean(LP)]
+  out[, LP := LP + MASS::mvrnorm(params$N, 0, resSig)]
+  return(out[, as.numeric(LP > (thresh + (z)*(sd.LP)))])
   
 }
 
@@ -78,7 +70,7 @@ simR <- function(params, X, Y, predsX){
     out <- rbinom(params$N, size = 1, prob = params$pm)
   } else {
     preds <- names(X)[grepl("^Rel", names(X))]
-    out <- simProbit(params, X[,preds], Y)
+    out <- simProbit(params, X[,..preds], Y)
   }
   return(list(R=out, predsR=preds))
 }
