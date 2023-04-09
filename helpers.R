6e8e0c29deefe9e546fc8618d83cd2a73c3cfcd0
GajaNen <33069162+GajaNen@users.noreply.github.com>
2023-04-10 02:01:24 +0300

    debugged
    removed unused helpers + debugged lambdaSE for sparseSVM

--- helpers.R
+++ helpers.R
@@ -1,22 +1,16 @@
 ###--------------------------------------------------------------------------###
 
-creatDumm <- function(dat) data.frame(apply(dat, 2,function(x) {model.matrix(~factor(x))[,-1]}))
-
-###--------------------------------------------------------------------------###
-
-extrReg <- function(rstr, symb="\\d+") as.numeric(unique(unlist(regmatches(rstr, regexpr(symb, rstr))))) 
-
-###--------------------------------------------------------------------------###
-
 lambdaSE <- function(x) 
 {
+  # lambdas ordered from largest to smallest
+  # lambda: regularization parameter (larger means larger penalty)
   index <- 1:length(x$cve)
-  bestIndex <- which.min(x$cve)
-  perf <- x$cve[bestIndex] + x$cve[bestIndex]
+  bestIndex <- which.min(x$cve) # take the largest lambda (first min) with the lowest ME
+  perf <- x$cve[bestIndex] + x$cvse[bestIndex]
   candidates <- index[x$cve <= perf]
-  best <- min(candidates)
+  best <- min(candidates) # take the smallest index (largest lambda)
   bestMetr <- x$cve[best]
-  return(list(best, bestMetr))
+  return(list(bestIndex=best, bestME=bestMetr))
 }
 
 ###--------------------------------------------------------------------------###
